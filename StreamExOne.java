f4b9eb39b0915244a76e02e26048f73f8f189d66
Nelson Moreno <thegreyabove@gmail.com>
2022-08-04 13:22:03 -0400

    initial commit

---
+++ StreamExOne.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class StreamExOne {
+
+    public static void main(String[] args) {
+		Stream<String> stream = Stream.of("one", "two", "three", "four", "five");
+
+		Predicate<String> p1 = s -> s.length() > 3;
+		Predicate<String> p2 =  Predicate.isEqual("two");
+		Predicate<String> p3 =  Predicate.isEqual("three");
+
+		stream
+			  .filter(p2.or(p3))
+			  .forEach(s -> System.out.println(s));
+
+	}
+
+}
